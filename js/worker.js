f75db152c41afe9395891809945f49f6ee65e5a7
seejwy <jinyoon@broadwayinc.com>
2023-08-18 12:27:52 +0900

    Rework pager structure and add insert and edit

--- js/worker.js
+++ js/worker.js
@@ -1,29 +1,79 @@
 self.addEventListener('message', (event) => {
+    const method = event.data.method;
     const list = event.data.list;
-    const index = event.data.index;
-    const maps = Object.keys(list.maps);
-    const newList = {
-        items: list.items,
-        maps: {}
-    }
-    maps.forEach(map => {
-        let set = new Set(list.maps[map]);
-        index.forEach((item) => {
+    const map = event.data.map;
+
+    if(method === 'delete') {
+        const index = event.data.index;
+
+        let set = new Set(map);
+        index.forEach(item => {
             if(set.has(item)) {
+                delete list.items[item];
                 set.delete(item);
             }
-        })
-
-        if(set.size) {
-            newList.maps[map] = [...set];
-        } else {
-            delete list.maps[map];
-        }
-    })
+        });
     
-    index.forEach(item => {
-        delete newList.items[item];
-    });
+        postMessage({
+            map: set.size ? [...set] : [],
+            list: list
+        })
+    } else if(method === 'insert') {
+        const items = event.data.items;
+        const sortBy = event.data.sortBy;
+        const order = event.data.order;
+        const id = event.data.id;
 
-    postMessage(newList);
+        items.forEach(item => {
+            list.items[item[id]] = item;
+
+            if(sortBy) {
+                let low = 0;
+                let high = map.length;
+        
+                while (low < high) {
+                    const mid = Math.floor((low + high) / 2);
+                    if(order === 'desc') {
+                        if (getSortByValue(item, sortBy) > getSortByValue(list.items[map[mid]], sortBy)) {
+                            high = mid;
+                        } else {
+                            low = mid + 1;
+                        }
+                    } else {
+                        if (getSortByValue(item, sortBy) < getSortByValue(list.items[map[mid]], sortBy)) {
+                            high = mid;
+                        } else {
+                            low = mid + 1;
+                        }
+                    }
+                }
+                
+                if((low >= map.length && list.endOfList) || low < map.length) {
+                    map.splice(low, 0, item[id]);
+                }
+            } else {
+                map.push(item[id])
+            }
+        });    
+
+        function getSortByValue(obj, path) {
+            const properties = path.split('.');
+            let currentObj = obj;
+
+            for (let prop of properties) {
+                if (currentObj.hasOwnProperty(prop)) {
+                    currentObj = currentObj[prop];
+                } else {
+                    return undefined; // or throw an error if you prefer
+                }
+            }
+
+            return currentObj;
+        }
+
+        postMessage({
+            map,
+            list: list
+        })    
+    }
 });
--- pager.ts
+++ pager.ts
@@ -1,110 +1,148 @@
+const worker = new Worker('./js/worker.js');
+
 export default class Pager {
+    _sortBy: string = null;
+    _order: 'asc' | 'desc';
+    _map: Array<string> = [];
     _list: {
-        maps: {
-            [params: string]: Array<string>;
-        };
         items: { [id: string]: Object };
-    };
+        endOfList: boolean
+    } = {
+            items: {},
+            endOfList: false
+        };
     id: string;
-    rpp: number
+    rpp: number;
 
     constructor(options: {
         id: string;
         resultsPerPage: number;
+        sortBy?: string;
+        order?: 'asc' | 'desc'
     }, list?: {
-        maps: {
-            [params: string]: Array<string>;
-        },
-        items: { [id: string]: Object };
+        map: Array<string>;
+        list: { [id: string]: Object };
+        endOfList: boolean;
     }) {
         this.id = options.id;
+        this._sortBy = options.sortBy;
+        this._order = options.order ? options.order : 'asc';
         this.rpp = options.resultsPerPage ? options.resultsPerPage : 10;
-        if (list) this._list = list;
-        else this._list = {
-            maps: {},
-            items: {}
-        };
-    }
 
-    sohash(params: Object): number {
-        let str = JSON.stringify(params);
-        let hash = 0;
-        for (let i = 0; i < str.length; i++) {
-            const char = str.charCodeAt(i);
-            hash = ((hash << 5) - hash) + char;
-            hash |= 0;
+        if (list) {
+            this._list = {
+                items: list.list,
+                endOfList: list.endOfList
+            };
+
+            this._map = list.map;
         }
-        return hash >>> 0;
     }
 
-    addList(params: Object, list: Array<Object>): {
+    addList(params: {
+        list: Array<Object>;
+        endOfList: boolean
+    }): {
         pages: number
     } {
-        let hash = this.sohash(params);
         let newItems = {};
-        let newMap: string[] = []
 
-        list.forEach(item => {
+        params.list.forEach(item => {
             newItems[item[this.id]] = item;
-            newMap.push(item[this.id]);
+            this._map.push(item[this.id])
         });
 
-        this._list.items = {
-            ...this._list.items,
-            ...newItems
-        };
-        if (this._list.maps[hash]) {
-            this._list.maps[hash] = [
-                ...this._list.maps[hash],
-                ...newMap
-            ];
-        } else {
-
-            this._list.maps[hash] = newMap;
-        }
+        Object.assign(this._list.items, newItems);
 
         return {
-            pages: Math.ceil(this._list.maps[hash].length / this.rpp)
+            pages: Math.ceil(this._map.length / this.rpp)
         }
     }
 
-    insertItem(params: Object, item: Object): void {
-        let hash = this.sohash(params);
-        this._list.items[item[this.id]] = item;
-        this._list.maps[hash].push(item[this.id]);
+    async editItems(items: { [key: string]: any }[]): Promise<"Edit Saved"> {
+        for (let i = 0; i < items.length; i++) {
+            const item = items[i];
+            const currentItem = this._list.items[item[this.id]];
+            
+            if (getSortByValue(currentItem, this._sortBy) !== getSortByValue(item, this._sortBy)) {
+                await this.delete([item[this.id]]);
+                await this.insertItems([item]);
+            }
+
+            this._list.items[item[this.id]] = item;
+        }
+
+        function getSortByValue(obj: { [key: string]: any }, path: string) {
+            const properties = path.split('.');
+            let currentObj = obj;
+
+            for (let prop of properties) {
+                if (currentObj.hasOwnProperty(prop)) {
+                    currentObj = currentObj[prop];
+                } else {
+                    return undefined;
+                }
+            }
+
+            return currentObj;
+        }
+
+        return "Edit Saved";
     }
 
-    getPage(params: Object, page: number): {
+    async insertItems(items: { [key: string]: any }[]): Promise<"Insert Successful"> {
+        worker.postMessage({
+            method: 'insert',
+            list: this._list,
+            map: this._map,
+            id: this.id,
+            sortBy: this._sortBy,
+            order: this._order,
+            items: items
+        });
+
+        return new Promise((res) => {
+            worker.onmessage = (event) => {
+                this._list = event.data.list;
+                this._map = event.data.map;
+
+                res("Insert Successful");
+            };
+        });
+
+
+    }
+
+    getPage(page: number): {
         list: Object[],
+        currentPage: number,
         maxPage: number
     } {
-        let hash = this.sohash(params);
         let startIndex = (page - 1) * this.rpp;
-        let result = this._list.maps[hash].slice(startIndex, startIndex + this.rpp);
+        let result = this._map.slice(startIndex, startIndex + this.rpp);
 
         return {
             list: result.map((item: string) => this._list.items[item]),
-            maxPage: Math.ceil(this._list.maps[hash].length / this.rpp)
+            currentPage: page,
+            maxPage: Math.ceil(this._map.length / this.rpp)
         };
     }
 
-    async delete(ids: string[]): Promise<string> {
-        const worker = new Worker('./js/worker.js');
-
+    async delete(ids: string[]): Promise<"Deleted"> {
         worker.postMessage({
+            method: 'delete',
             list: this._list,
+            map: this._map,
             index: ids
         });
 
-        let workerResult = new Promise((res) => {
+        return new Promise((res) => {
             worker.onmessage = (event) => {
-                this._list.items = event.data.items;
-                this._list.maps = event.data.maps;
+                this._list = event.data.list;
+                this._map = event.data.map;
 
                 res("Deleted");
             };
         });
-
-        return await workerResult as Promise<string>;
     }
 }
